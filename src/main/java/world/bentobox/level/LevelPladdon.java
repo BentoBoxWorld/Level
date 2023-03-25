c093796a6eea80ffa3d7d68fc641c65ed27f7733
tastybento <tastybento@wasteofplastic.com>
2023-03-25 09:53:41 -0700

    Remove dependency

--- src/main/java/world/bentobox/level/LevelPladdon.java
+++ src/main/java/world/bentobox/level/LevelPladdon.java
@@ -1,6 +1,5 @@
 package world.bentobox.level;
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
@@ -12,9 +11,8 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  * @author tastybento
  *
  */
-@Plugin(name="Pladdon", version="1.0")
+@Plugin(name="Level", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_16)
-@Dependency(value = "BentoBox")
 public class LevelPladdon extends Pladdon {
     @Override
     public Addon getAddon() {
