f92becc8ac76d8d2cdd801559c8982fb6884c1d6
BONNe <bonne@bonne.id.lv>
2023-04-08 18:20:29 +0300

    Remove Spigot Plugin Annotations

--- src/main/java/world/bentobox/level/LevelPladdon.java
+++ src/main/java/world/bentobox/level/LevelPladdon.java
@@ -1,8 +1,5 @@
 package world.bentobox.level;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
@@ -11,8 +8,6 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  * @author tastybento
  *
  */
-@Plugin(name="Level", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_16)
 public class LevelPladdon extends Pladdon {
     @Override
     public Addon getAddon() {
