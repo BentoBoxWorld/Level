76e0bad88a56f0b9d7ef2da8280b2f410e1ee729
tastybento <tastybento@users.noreply.github.com>
2024-01-13 08:50:37 -0800

    Added test class for EquationEvaluator

--- src/main/java/world/bentobox/level/calculators/EquationEvaluator.java
+++ src/main/java/world/bentobox/level/calculators/EquationEvaluator.java
@@ -3,119 +3,119 @@ package world.bentobox.level.calculators;
 import java.text.ParseException;
 
 /**
- * @author tastybento
+ * Utility class to evaluate equations
  */
 public class EquationEvaluator {
 
     private static class Parser {
-	private final String input;
-	private int pos = -1;
-	private int currentChar;
+        private final String input;
+        private int pos = -1;
+        private int currentChar;
 
-	public Parser(String input) {
-	    this.input = input;
-	    moveToNextChar();
-	}
+        @SuppressWarnings("unused")
+        private Parser() {
+            throw new IllegalStateException("Utility class");
+        }
 
-	private void moveToNextChar() {
-	    currentChar = (++pos < input.length()) ? input.charAt(pos) : -1;
-	}
+        public Parser(String input) {
+            this.input = input;
+            moveToNextChar();
+        }
 
-	private boolean tryToEat(int charToEat) {
-	    while (currentChar == ' ')
-		moveToNextChar();
-	    if (currentChar == charToEat) {
-		moveToNextChar();
-		return true;
-	    }
-	    return false;
-	}
+        private void moveToNextChar() {
+            currentChar = (++pos < input.length()) ? input.charAt(pos) : -1;
+        }
 
-	public double evaluate() throws ParseException {
-	    double result = parseExpression();
-	    if (pos < input.length()) {
-		throw new ParseException("Unexpected character: " + (char) currentChar, pos);
-	    }
-	    return result;
-	}
+        private boolean tryToEat(int charToEat) {
+            while (currentChar == ' ') {
+                moveToNextChar();
+            }
+            if (currentChar == charToEat) {
+                moveToNextChar();
+                return true;
+            }
+            return false;
+        }
 
-	private double parseExpression() throws ParseException {
-	    double result = parseTerm();
-	    while (true) {
-		if (tryToEat('+'))
-		    result += parseTerm();
-		else if (tryToEat('-'))
-		    result -= parseTerm();
-		else
-		    return result;
-	    }
-	}
+        public double evaluate() throws ParseException {
+            double result = parseExpression();
+            if (pos < input.length()) {
+                throw new ParseException("Unexpected character: " + (char) currentChar, pos);
+            }
+            return result;
+        }
 
-	private double parseFactor() throws ParseException {
-	    if (tryToEat('+'))
-		return parseFactor(); // unary plus
-	    if (tryToEat('-'))
-		return -parseFactor(); // unary minus
+        private double parseExpression() throws ParseException {
+            double result = parseTerm();
+            while (true) {
+                if (tryToEat('+')) {
+                    result += parseTerm();
+                } else if (tryToEat('-')) {
+                    result -= parseTerm();
+                } else {
+                    return result;
+                }
+            }
+        }
 
-	    double x;
-	    int startPos = this.pos;
-	    if (tryToEat('(')) { // parentheses
-		x = parseExpression();
-		tryToEat(')');
-	    } else if ((currentChar >= '0' && currentChar <= '9') || currentChar == '.') { // numbers
-		while ((currentChar >= '0' && currentChar <= '9') || currentChar == '.')
-		    moveToNextChar();
-		x = Double.parseDouble(input.substring(startPos, this.pos));
-	    } else if (currentChar >= 'a' && currentChar <= 'z') { // functions
-		while (currentChar >= 'a' && currentChar <= 'z')
-		    moveToNextChar();
-		String func = input.substring(startPos, this.pos);
-		x = parseFactor();
-		switch (func) {
-		case "sqrt":
-		    x = Math.sqrt(x);
-		    break;
-		case "sin":
-		    x = Math.sin(Math.toRadians(x));
-		    break;
-		case "cos":
-		    x = Math.cos(Math.toRadians(x));
-		    break;
-		case "tan":
-		    x = Math.tan(Math.toRadians(x));
-		    break;
-		case "log":
-		    x = Math.log(x);
-		    break;
-		default:
-		    throw new ParseException("Unknown function: " + func, startPos);
-		}
-	    } else {
-		throw new ParseException("Unexpected: " + (char) currentChar, startPos);
-	    }
+        private double parseFactor() throws ParseException {
+            if (tryToEat('+')) {
+                return parseFactor(); // unary plus
+            }
+            if (tryToEat('-')) {
+                return -parseFactor(); // unary minus
+            }
+            double x;
+            int startPos = this.pos;
+            if (tryToEat('(')) { // parentheses
+                x = parseExpression();
+                tryToEat(')');
+            } else if ((currentChar >= '0' && currentChar <= '9') || currentChar == '.') { // numbers
+                while ((currentChar >= '0' && currentChar <= '9') || currentChar == '.') {
+                    moveToNextChar();
+                }
+                x = Double.parseDouble(input.substring(startPos, this.pos));
+            } else if (currentChar >= 'a' && currentChar <= 'z') { // functions
+                while (currentChar >= 'a' && currentChar <= 'z') {
+                    moveToNextChar();
+                }
+                String func = input.substring(startPos, this.pos);
+                x = parseFactor();
+                x = switch (func) {
+                case "sqrt" -> Math.sqrt(x);
+                case "sin" -> Math.sin(Math.toRadians(x));
+                case "cos" -> Math.cos(Math.toRadians(x));
+                case "tan" -> Math.tan(Math.toRadians(x));
+                case "log" -> Math.log(x);
+                default -> throw new ParseException("Unknown function: " + func, startPos);
+                };
+            } else {
+                throw new ParseException("Unexpected: " + (char) currentChar, startPos);
+            }
 
-	    if (tryToEat('^'))
-		x = Math.pow(x, parseFactor()); // exponentiation
+            if (tryToEat('^')) {
+                x = Math.pow(x, parseFactor()); // exponentiation
+            }
 
-	    return x;
-	}
+            return x;
+        }
 
-	private double parseTerm() throws ParseException {
-	    double x = parseFactor();
-	    for (;;) {
-		if (tryToEat('*'))
-		    x *= parseFactor(); // multiplication
-		else if (tryToEat('/'))
-		    x /= parseFactor(); // division
-		else
-		    return x;
-	    }
-	}
+        private double parseTerm() throws ParseException {
+            double x = parseFactor();
+            for (;;) {
+                if (tryToEat('*'))
+                    x *= parseFactor(); // multiplication
+                else if (tryToEat('/'))
+                    x /= parseFactor(); // division
+                else
+                    return x;
+            }
+        }
 
     }
 
     public static double eval(final String equation) throws ParseException {
-	return new Parser(equation).evaluate();
+        return new Parser(equation).evaluate();
     }
 
 }
