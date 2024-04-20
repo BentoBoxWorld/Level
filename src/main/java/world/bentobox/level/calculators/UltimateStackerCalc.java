a4f8d121389ac833839c13a9b92d6c7d9509ddde
tastybento <tastybento@users.noreply.github.com>
2024-04-20 09:50:17 -0700

    Isolate UltimateStacker imports so no errors if US is not installed (#307)
    Fixes #306

---
+++ src/main/java/world/bentobox/level/calculators/UltimateStackerCalc.java
@@ -0,0 +1,29 @@
+package world.bentobox.level.calculators;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+import com.craftaro.ultimatestacker.api.UltimateStackerApi;
+import com.craftaro.ultimatestacker.api.utils.Stackable;
+
+import world.bentobox.bentobox.BentoBox;
+
+/**
+ * Isolates UltimateStacker imports so that they are only loaded if the plugin exists
+ */
+public class UltimateStackerCalc {
+    public static void addStackers(Material material, Location location, Results results, boolean belowSeaLevel,
+            int value) {
+        Stackable stack = UltimateStackerApi.getBlockStackManager().getBlock(location);
+        if (stack != null) {
+            if (belowSeaLevel) {
+                results.underWaterBlockCount.addAndGet((long) stack.getAmount() * value);
+                results.uwCount.add(material);
+            } else {
+                results.rawBlockCount.addAndGet((long) stack.getAmount() * value);
+                results.mdCount.add(material);
+            }
+        }
+    }
+}
+
